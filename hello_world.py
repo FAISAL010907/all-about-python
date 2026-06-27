9dfa06aa02e3da8eaa2abfc50cd0546d5b6103f7
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-06-27 18:20:11 +0530

    hellow world

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+git config --global user.name "FAISAL010907"
+
+
+
+
+
+
+#to write something in python we use print(xyz)
+print("Hello, World!")
+
+"""if you want to write multi line comment 
+ in python you can use triple quotes like 
+this"""
+
+#use print() to print multiple limes
+
+print("""Starting my Python learning journey today,
+      and I am documenting every step of the way! 
+      It feels incredible to write code that actually works, 
+      starting with simple print statements. 
+      My goal is to log my progress daily, 
+      from mastering loops to building my own projects. 
+      Every error I encounter is just an opportunity to learn,
+      solve problems, and grow. 
+      I am excited to see how far these new coding skills will 
+      take me in the future!""")
