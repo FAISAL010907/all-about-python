a0294a5ab778ed112fd476194869df413399ca52
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-07-06 21:31:07 +0530

    new

---
+++ chapter 2/chapter 2 PS/01_problem.py
@@ -0,0 +1,5 @@
+a = 1 
+b = 2 
+print("the sum of two number is:", int(a)+int(b))
+      
+input("your number",)
--- chapter 2/chapter 2 variables and data types/01_variables.py
+++ chapter 2/chapter 2 variables and data types/01_variables.py
@@ -3,3 +3,16 @@ a = 1
 b = 2
 name = "faisal"
 print(a+b)
+
+print("==========================")
+
+username = 'faisal kamal'
+level = 999
+course_progress = 0.92
+pet_alive = False
+print("------------------")
+
+print(username)
+print(level)
+print(course_progress)
+print(pet_alive)
