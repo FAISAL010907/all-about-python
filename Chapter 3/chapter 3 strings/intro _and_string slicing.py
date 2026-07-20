0812d55e66aa201733efe39c83bb98db76935ae9
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-07-20 15:08:53 +0530

    strings

---
+++ Chapter 3/chapter 3 strings/intro _and_string slicing.py
@@ -0,0 +1,21 @@
+# 3 ways to write
+#single string
+name = 'faisal'
+#double string
+name = "faisal"
+# triple string
+name = '''faisal'''
+
+#string slicing
+name = "faisal"
+
+nameshort = name[0:4]
+
+print(nameshort)
+
+character1 = name[3]
+print(character1)
+character2 = name[2]
+print(character2)
+character3 = name[4]
+print(character3)
