832f802f20be6783e3ff2075b04ea3be2e7eeedd
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-08-09 02:06:58 +0530

    problem solving

---
+++ Chapter 6/Chapter 6 ps/01_problem 01.py
@@ -0,0 +1,24 @@
+# QUESTION 1 ))
+"""1. Write a program to find the greatest of four numbers entered by the user """
+
+#  SOLUTION 1 )
+
+a = int(input("enter your number : "))
+b = int(input("enter your number : "))
+c = int(input("enter your number : "))
+d = int(input("enter your number : "))
+
+if(a>b and a>c and a>d):
+    print("the greaters number is a :", a)
+
+elif(b>a and b>c and b>d):
+    print("the greaters number is b :", b)
+
+elif(c>b and c>a and c>d):
+    print("the greaters number is c :", c)
+
+elif(d>b and d>c and d>a):
+    print("the greaters number is d :", d)
+else:
+    print("all the input numbers are same")
+
