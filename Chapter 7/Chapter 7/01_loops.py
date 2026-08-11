9674ddfc1268f647823d8562bc83a0973346f2dc
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-08-12 00:54:37 +0530

    loopw

---
+++ Chapter 7/Chapter 7/01_loops.py
@@ -0,0 +1,16 @@
+#  LOOPS 
+# loops help when we want to tell computer which line we want to repeate and how to 
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+print('====================================')
+
+# for more precise code we can use loops
+for i in range(1,50):
+    print(i+1)
+
+print('====================================')
+
---
+++ Chapter 7/Chapter 7/02_while_loops.py
@@ -0,0 +1,30 @@
+i = 1
+
+while(i<6):
+    print(i)
+    i += 1
+print('====================================')
+
+i = 0 
+while i<5:
+    print("faisal")
+    i = i+1
+
+print('====================================')
+
+# QUESTION 1)
+
+# write a program to print 1 to 100 using loop 
+for i in range (1,100):
+    print(i+1)
+
+print('====================================')
+
+# QUESTION 2))
+# write a program to print the content of a list using while loop
+list = ["faisal", "ali", "ahmed", "hassan", "hussain"]
+i = 0
+while i < len(list):
+    print(list[i])
+    i += 1
+    
