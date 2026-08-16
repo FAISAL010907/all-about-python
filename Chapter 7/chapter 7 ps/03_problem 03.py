8d10d1a7d127675100b3142193a783245e8f2587
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-08-16 18:21:15 +0530

    problem solving

---
+++ Chapter 7/chapter 7 ps/03_problem 03.py
@@ -0,0 +1,12 @@
+# QUESTION 3 ))
+'''Attempt problem 1 using while loop'''
+
+# SOLUTION :- 
+
+P = int(input("enter number for patter :  "))
+
+i = 1
+while  (i <=10) :
+     print(f"{P} times {i} = {P*i}")
+     i += 1
+ 
