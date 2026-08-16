0b1d1309586b7ca0cabb59732e3b4c188ef90709
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-08-16 20:52:33 +0530

    * pattern question

---
+++ Chapter 7/chapter 7 ps/07_problem 07.py
@@ -0,0 +1,12 @@
+# QUESTION 7 ))
+'''7. Write a program to print the following star pattern.
+         *
+        ***
+       ***** for n = 3'''
+
+# SOLUTION :-
+n = int(input("enter your number : "))
+for i in range (1,n+1):
+    print(" "* (n-i), end="")
+    print("*"* (2*i-1), end ="")
+    print("")
