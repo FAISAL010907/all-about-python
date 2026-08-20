59205644f9d9477aecbc5394bbdc7a1f1d2fe86d
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-08-20 20:47:23 +0530

    temp conversion using function

---
+++ Chapter 8/Chapter 8 ps/02_problem 02.py
@@ -0,0 +1,32 @@
+# QUESTION 2 ))
+'''Write a python program using function to convert Celsius to Fahrenheit'''
+
+# SOLUTION 1:-
+
+def celsius():
+    c = float(input("enter the temperaure in Celcius : "))
+    fahrenheit = (c*(9/5)+32)
+    kelvin = (c + 273.15)
+
+    print(f"'{c}°C' celsius is '{fahrenheit}°F' Fahrenheit")
+    print(f"'{c}°C' celsius is '{kelvin}°K' Kelvin")
+
+def kelvin():
+    k = float(input("enter the temperature in Kelvin : "))
+    celsius = (k - 273.15)
+    fahrenheit = (k-273.15)*(9/5)+32
+
+    print(f"'{k}°K' kelvin is '{celsius:.2f}°C' celsius")
+    print(f"'{k}°K' kelvin is '{fahrenheit:.2f}°F' fahrenheit")
+
+def fahrenheit():
+    f = float(input("enter the temperature in Fahrenheit : "))
+    celsius = (f-32)*(5/9)
+    kelvin = (f-32)*(5/9)+273.15
+
+    print(f"'{f}°F' fahrenheit is '{celsius:.2f}°C' Celsius")
+    print(f"'{f}°F' fahrenheit is '{kelvin:.2f}°K' Kelvin")
+
+celsius()
+kelvin()
+fahrenheit()
