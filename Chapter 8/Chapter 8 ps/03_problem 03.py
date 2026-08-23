f29ca6cbfac2ebd10d2b34a6d6b04251825b77f1
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-08-23 18:33:24 +0530

    recursiom

---
+++ Chapter 8/Chapter 8 ps/03_problem 03.py
@@ -0,0 +1,5 @@
+#  QUESTION 3 ))
+'''How do you prevent a python print() function to print a new line at the end.'''
+
+# SOLUTION :-
+
---
+++ Chapter 8/Chapter 8/04_recursion.py
@@ -0,0 +1,17 @@
+''' 0! = 1
+    1! = 1
+    
+    5! = 1*2*3*4*5 
+    
+    n! = n * (n-1)* (n-2).........(n-n)
+    
+    Factorial (n) = n * factorial(n-1)
+    '''
+def factorial(n):     # in place of factorial or we casually say fxn we can give any name to the fxn
+    if n == 0 or n == 1: # if we dont do this the fxn wont stop if we put input as 0 or 1 and it will become a infinite loop
+        return 1
+    return n*factorial(n-1)
+
+n = int(input('enter your number : '))
+print("the factorial value of your input is",factorial(n))    # in place of factorial or we casually say fxn we can give any name to the fxn
+
