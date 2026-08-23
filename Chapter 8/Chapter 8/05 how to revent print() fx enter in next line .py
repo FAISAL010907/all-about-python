439a5677e5f6fbb2a0a30cdfa2a7b0661c0c1020
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-08-23 19:37:17 +0530

    new solutiion

---
+++ Chapter 8/Chapter 8/05 how to revent print() fx enter in next line .py
@@ -0,0 +1,23 @@
+#  HOW TO PREVENT PRINT() FNT TO ENTER IN NEXT LINE 
+
+'''We can do it using ( end = "" )'''
+
+print("hello normal")
+# print hello normal
+print("==========================================")
+
+
+print("hello" ,end="")
+# this end fxn will he print fxn to enter to next line
+print("==========================================")
+
+
+print("hello", end = " ")
+print("world!!")
+#  [ hello world!! ]  and we can add anything in "" these of end fxn to add between two line of code we want to to bring in one line
+
+print("==========================================")
+
+print("Hello")          
+print("kasa kai", end=" ")
+print(" faisal ", end=" bahi ")  
