068c9a60519684f802b5a1401929dbbeb6376eec
FAISAL010907 <faisalshaikh2098@gmail.com>
2026-09-01 02:51:09 +0530

    modify pyttx for my bdy

--- chapter 1/chapter 1 PS/voice module.py
+++ chapter 1/chapter 1 PS/voice module.py
@@ -2,5 +2,14 @@
 
 import pyttsx3
 engine = pyttsx3.init()
-engine.say(" MAKAA LAADLEY MEOW GHOAP GHOAP GHOAP ")
+engine.say(''' Happy Birthday, Faisal!
+
+I want to wish you the absolute warmest, happiest birthday today! You deserve a year filled with genuine peace, 
+immense joy, healthy connections, 
+and huge success in everything you set out to do.
+
+May this new chapter bring you all the warmth, happiness, and opportunities you've been hoping for. 
+Keep being awesome, keep creating, 
+and enjoy every single moment of your special day!
+HAPPY BIRTHDAY AGAIN. ''')
 engine.runAndWait()
